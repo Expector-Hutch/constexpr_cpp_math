5da03a0edd92ae31a992a0cc4b8a75dcdd060ee4
Expector <expector-hutch@outlook.com>
2025-01-17 21:48:45 +0800

    Create constexpr_math.hpp
    make a basic hpp file

---
+++ constexpr_math.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef CONSTEXPR_MATH_HPP
+#define CONSTEXPR_MATH_HPP
+
+#include <type_traits>
+
+namespace constexpr_math {
+} // namespace constexpr_math
+
+#endif // CONSTEXPR_MATH_HPP
