7cf373f302cbd79fb41f0cd5b90746907275c223
Expector <expector-hutch@outlook.com>
2025-01-18 13:36:35 +0800

    add constants
    add some useful constants including pi, phi, e, etc.

--- constexpr_math.hpp
+++ constexpr_math.hpp
@@ -6,6 +6,73 @@
 
 namespace constexpr_math {
 
+inline namespace constant {
+
+// π = circumference/diameter
+template <typename number_t>
+constexpr number_t pi = number_t(
+    3.14159265358979323846264338327950288419716939937510582097494459230781640628620899L);
+// τ = 2π
+template <typename number_t>
+constexpr number_t tau = number_t(
+    6.28318530717958647692528676655900576839433879875021164194988918461563281257241799L);
+// π⁻¹ = 1/π
+template <typename number_t>
+constexpr number_t inv_p = number_t(
+    0.318309886183790671537767526745028724068919291480912897495334688117470859573164L);
+// π⁻² = 1/√π
+template <typename number_t>
+constexpr number_t inv_sqrtpi = number_t(
+    0.564189583547756286948079451560772585844050629329000826876428277587516887844419L);
+
+// e = base of natural logarithm
+template <typename number_t>
+constexpr number_t e = number_t(
+    2.71828182845904523536028747135266249775724709369995957496696762772407663035354759L);
+// log₂e
+template <typename number_t>
+constexpr number_t log2e = number_t(
+    1.442695040888963407359924681001892137426645954152985934135449406931219160354371L);
+// log₁₀e
+template <typename number_t>
+constexpr number_t log10e = number_t(
+    0.434294481903251827651128918916605082294397005803666566114453655241094391594651L);
+// ln2
+template <typename number_t>
+constexpr number_t ln2 = number_t(
+    0.693147180559945309417232121458176568075500134360255254120680009493393621969694L);
+// ln10
+template <typename number_t>
+constexpr number_t ln10 = number_t(
+    2.302585092994045684017991454684364207601101488628772976033327900967572609677352L);
+
+// √2
+template <typename number_t>
+constexpr number_t sqrt2 = number_t(
+    1.41421356237309504880168872420969807856967187537694807317667973799073247846210704L);
+// √3
+template <typename number_t>
+constexpr number_t sqrt3 = number_t(
+    1.73205080756887729352744634150587236694280525381038062805580697945193301690880037L);
+// 3⁻² = 1/√3
+template <typename number_t>
+constexpr number_t inv_sqrt3 = number_t(
+    0.577350269189625764509148780501957455647601751270126876018602326483977672437250L);
+
+// γ = Euler's constant
+template <typename number_t>
+constexpr number_t egamma = number_t(
+    0.57721566490153286060651209008240243104215933593992359880576723488486772677766467L);
+
+// φ = golden ratio = (1 + √5)/2
+template <typename number_t>
+constexpr number_t phi = number_t(
+    1.61803398874989484820458683436563811772030917980576286213544862270526046281890245L);
+
+} // namespace constant
+
+inline namespace function {
+
 template <typename T>
 constexpr T min(const T &a, const T &b) {
     return a < b ? a : b;
@@ -78,6 +145,8 @@ constexpr base_t pow(base_t base, exponent_t exponent) {
     }
     // todo
 }
+
+} // namespace function
 
 } // namespace constexpr_math
 
